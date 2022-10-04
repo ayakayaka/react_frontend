80eb27461eda8a0d293a6d69a0b635c4cc8a95a9
Ayaka Inaba <ayakayakaaaa@gmail.com>
2022-10-04 22:59:12 +0900

    still

--- react_frontend/src/App.js
+++ react_frontend/src/App.js
@@ -9,7 +9,7 @@ import {
 import Users from "./user/pages/Users";
 import NewPlace from "./places/pages/NewPlace";
 import MainNavigation from "./shared/components/Navigation/MainNavigation";
-import MainNavigation from "./shared/components/Navigation/MainNavigation";
+import UserPlaces
 
 const App = () => {
   return (
